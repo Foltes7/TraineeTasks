f6d3678ecb9e93cfce7ba5f7ae8dd37578ed1973
Davyd Konopatskyi <davydqq@gmail.com>
2021-01-28 23:16:54 +0200

    add new customer

--- Angular_NET_CORE/ClientApp/src/app/customers/new-customer/new-customer.component.ts
+++ Angular_NET_CORE/ClientApp/src/app/customers/new-customer/new-customer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-customer',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewCustomerComponent implements OnInit {
 
-  constructor() { }
+  public mainForm: FormGroup = new FormGroup({
+    date: new FormControl('',  [Validators.required]),
+    name: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(45) ]),
+    address: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(45) ]),
+  });
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+     const today = new Date().toISOString().split('T')[0];
+     this.date?.setValue(today);
+  }
+
+  get date()
+  {
+    return this.mainForm.get('date');
+  }
+  get name() {
+    return this.mainForm.get('name');
+  }
+
+  get address() {
+    return this.mainForm.get('address');
+  }
+
+  goToCustomersPage(): void
+  {
+    this.router.navigate(['customers']);
+  }
+
+  createNewCustomer()
+  {
+    console.log(this.mainForm.value);
   }
 
 }
--- Angular_NET_CORE/ClientApp/src/app/shared/shared.module.ts
+++ Angular_NET_CORE/ClientApp/src/app/shared/shared.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
+import {MatInputModule} from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
-    MatTableModule
+    MatTableModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
-  exports: [MatButtonModule, MatTableModule]
+  exports: [MatButtonModule, MatTableModule, MatInputModule, ReactiveFormsModule]
 })
 export class SharedModule { }
