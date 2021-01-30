b2dce39cf6e696614d11289961e8a27149209f0f
Davyd Konopatskyi <davydqq@gmail.com>
2021-01-30 16:09:00 +0200

    init console test task

---
+++ ConsoleTestTask/ConsoleTestTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleTestTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
